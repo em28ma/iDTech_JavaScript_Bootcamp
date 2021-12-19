c9a46ce856f080a04351c448b568611f2b7b126b
em28ma <rapilat.emma@gmail.com>
2021-12-19 21:03:10 +0100

    new file

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,34 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Greetings! Welcome to gambling school";
+let name = "Mary"
+let title = " Pet"
+let EventNAme = "Welcoming games"
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let money = 1000000000;
+let debt = 5000000;
+let Stuents = 2500;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let Games = [" BlackJack"," Indian poker"," Memory", " Russian roulette"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+EventStarted = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + ", you must be " + name + "!");
+console.log("In this school all we do is gamble, day and night that's how we earn and loose money, there are " + Stuents + " Students");
+console.log("For example, right now I have " + money + " yens");
+console.log("If you loose money at this school you become a" + title + ". Being a pet is horrible, people will use you and you will have to pay your debt.");
+console.log("For exemple this person right there is Sarah she's been a pet for over one year now and still has a debt of " + debt +" yens to pay.");
+console.log("Anyways, new sudents come here every year, so we host our annual " + EventNAme + "!");
+console.log("me : what are the " + EventNAme + "?");
+console.log("Good question!! This event is mainly gambling! you could choose the games you would like to play!");
+console.log("We have " + Games +" and many more!!");
+console.log("me : Ohhh awsome I really like"+ Games[1] + "! " + "Is it true? the games already started? ");
+console.log(EventStarted +", they started 15 min ago! GO quick!! And good luck, be careful, nobody here is completly honest...")
